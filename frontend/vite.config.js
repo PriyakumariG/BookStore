5cbdf6b44dfcfacc0f1c0c70e0b20077976ef2f4
Priyakumari Gupta <146571952+PriyakumariG@users.noreply.github.com>
2025-05-24 16:07:34 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 2000, // increase chunk size limit to suppress warning
+  },
+});
